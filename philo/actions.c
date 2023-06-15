12f2c5810b7eea4357de557bcdca9c93f7cc288d
Lorenzo Nicotera <lnicoter@e3r6p3.42roma.it>
2023-06-15 16:48:45 +0200

    mandatory apposto

---
+++ philo/actions.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   actions.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlongo <mlongo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/13 12:02:07 by mlongo            #+#    #+#             */
+/*   Updated: 2023/06/14 16:39:34 by mlongo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Philosophers.h"
+
+void	died(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->data->write);
+	printf("%llu %d is died\n", get_time() - philo->data->start_time, philo->id);
+}
+
+void	eat(t_philo *philo)
+{
+	pthread_mutex_lock(philo->l_fork);
+	has_taken_a_fork(philo);
+	pthread_mutex_lock(philo->r_fork);
+	has_taken_a_fork(philo);
+	pthread_mutex_lock(&philo->lock);
+	pthread_mutex_lock(&philo->data->lock);
+	philo->eat_cont++;
+	philo->eating = 1;
+	philo->time_to_die = get_time() + philo->data->death_time;
+	pthread_mutex_unlock(&philo->lock);
+	pthread_mutex_unlock(&philo->data->lock);
+	pthread_mutex_lock(&philo->data->write);
+	printf("%llu %d is eating\n", get_time()
+		- philo->data->start_time, philo->id);
+	pthread_mutex_unlock(&philo->data->write);
+	ft_usleep(philo->data->eat_time);
+	pthread_mutex_lock(&philo->lock);
+	philo->eating = 0;
+	pthread_mutex_unlock(&philo->lock);
+	pthread_mutex_unlock(&philo->lock);
+	pthread_mutex_unlock(philo->l_fork);
+	pthread_mutex_unlock(philo->r_fork);
+}
+
+void	has_taken_a_fork(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->data->write);
+	printf("%llu %d has taken a fork\n", get_time()
+		- philo->data->start_time, philo->id);
+	pthread_mutex_unlock(&philo->data->write);
+}
+
+void	mine_sleep(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->data->write);
+	printf("%llu %d is sleeping\n", get_time()
+		- philo->data->start_time, philo->id);
+	pthread_mutex_unlock(&philo->data->write);
+	ft_usleep(philo->data->sleep_time);
+}
+
+void	thinking(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->data->write);
+	printf("%llu %d is thinking\n", get_time()
+		- philo->data->start_time, philo->id);
+	pthread_mutex_unlock(&philo->data->write);
+}
---
+++ philo/threads.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   threads.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlongo <mlongo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/12 18:28:12 by mlongo            #+#    #+#             */
+/*   Updated: 2023/06/14 16:37:18 by mlongo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Philosophers.h"
+
+void	*routinemonitor(void *data2)
+{
+	int		i;
+	t_data	*data;
+
+	i = 0;
+	data = (t_data *)data2;
+	while (!data->finished && !data->dead)
+	{
+		i = 0;
+		while (i < data->philo_num)
+		{
+			if (data->philos[i].eat_cont < data->meals_nb)
+				break ;
+			i++;
+		}
+		if (i == data->philo_num)
+		{
+			pthread_mutex_lock(&data->lock);
+			data->finished = 1;
+			ft_usleep(10);
+			pthread_mutex_unlock(&data->lock);
+		}
+	}
+	return (NULL);
+}
+
+void	*routine_supervisor(void *philo2)
+{
+	int		i;
+	t_philo	*philo;
+
+	i = 0;
+	philo = (t_philo *)philo2;
+	while (!philo->data->dead && !philo->data->finished)
+	{
+		pthread_mutex_lock(&philo->lock);
+		if ((get_time() >= philo->time_to_die) && !philo->eating)
+		{
+			pthread_mutex_unlock(&philo->lock);
+			pthread_mutex_lock(&philo->data->lock);
+			died(philo);
+			philo->data->dead = 1;
+			ft_usleep(10000);
+			pthread_mutex_unlock(&philo->data->write);
+			pthread_mutex_unlock(&philo->data->lock);
+			break ;
+		}
+		pthread_mutex_unlock(&philo->lock);
+	}
+	return (NULL);
+}
+
+void	*routine_philo(void	*philo2)
+{
+	int		i;
+	t_philo	*philo;
+
+	i = 0;
+	philo = (t_philo *)philo2;
+	if (pthread_create(&philo->t1, NULL, &routine_supervisor, (void *)philo))
+		return (NULL);
+	while (!philo->data->dead && !philo->data->finished)
+	{
+		eat(philo);
+		mine_sleep(philo);
+		thinking(philo);
+	}
+	return (NULL);
+}
